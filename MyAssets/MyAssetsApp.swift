7fbb5d82576b5fe2b94dc04e7732b6824c104f73
koohee <ko_ohee@naver.com>
2022-06-02 22:14:26 +0900

    Initial Commit

---
+++ MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 구희정 on 2022/06/02.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
